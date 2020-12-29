b1f59be917a32c96a8580b65afb2ccf2d4fcf2af
Francisca999-tina <68696276+Francisca999-tina@users.noreply.github.com>
2020-12-28 21:10:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const app = express ()
+const app = express()
 const port = 3000
 
-app.get('/', (req, res) => rest.send('Óla mundo pelo Express!'))
+app.get('/', (req, res) => res.send('Óla mundo pelo Express!'))
 
 app.listen(port,() => console.log('Api rodando na porta 3000'))
